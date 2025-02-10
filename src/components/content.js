5c9e3a6b5b1f47af7b7603240a85589a3c48204b
wiemfeddaoui <wiemfeddaoui99@gmail.com>
2025-02-10 15:04:51 +0000

    Added handleCkeck and handleDelete, updated css

--- src/components/content.js
+++ src/components/content.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
+import { FaTrashAlt } from 'react-icons/fa';
 
 const Content = () => {
 
-    const {items, setItems} = useState([
+    const [items, setItems] = useState([
         {
             id: 1,
             checked: false,
@@ -10,8 +11,8 @@ const Content = () => {
         },
         {
             id: 2,
-            checked: false,
-            item: "pasta",
+            checked: true,
+            item: "pasta khjk hghjk ghjkl vghjki fghj dtfgyhjk fghujik",
         },
         {
             id: 3,
@@ -20,11 +21,44 @@ const Content = () => {
         }
     ]);
 
+    const handleCheck = (id) => {
+        const newList = items.map((item) =>
+        id === item.id ? {...item, checked: !item.checked} : item
+        );
+        setItems(newList);
+    }
+
+    const handleDelete = (id) => {
+        const newList = items.filter((item) =>
+        id !== item.id
+        );
+        setItems(newList);
+    }
+
     return (
         <main className="content">
+          
+           {items.length ? (<ul>
+            {items.map((item) => (
+                <li key={item.id} className="item">
+                    <input
+                    type="checkbox"
+                    checked={item.checked} 
+                    onChange={() => handleCheck(item.id)}
+                     />
+                    <label>{item.item}</label>
+                    <FaTrashAlt
+                        onClick={() => handleDelete(item.id)}
+                        role="button"
+                        tabIndex="0"
+                    />
+                </li>
+                ))}
+              </ul>) : <h3 className="noItem"> No items in groceries list! </h3>
+            }
             
         </main>
         )
-}
+};
 
 export default Content;
